e834bd925abd46929c98af732a3a73e7a1f84972
dbsrudekf <88525042+dbsrudekf@users.noreply.github.com>
2022-01-24 23:52:38 +0900

    Player Jump기능 추가
    Player Jump기능만 추가 애니메이션x

--- 2D_Mario/Assets/Scripts/Player.cs
+++ 2D_Mario/Assets/Scripts/Player.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     float fSpeed = 5.0f;
+    public float jumpPower = 10.0f;
 
     Rigidbody rRigidbody;
     Vector3 vMovement;
@@ -26,6 +27,11 @@ public class Player : MonoBehaviour
     {
         if (Input.GetButtonDown("Horizontal"))
             spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            rRigidbody.AddForce(Vector2.up * jumpPower, ForceMode.Impulse);
+        }
 
         if (Input.GetButtonUp("Horizontal"))
             anim.SetBool("IsWalkingFinish", true);
