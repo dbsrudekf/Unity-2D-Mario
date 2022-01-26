7c8b14233949b1722ad4f9b9af97d0a99976a361
dbsrudekf <88525042+dbsrudekf@users.noreply.github.com>
2022-01-26 23:59:12 +0900

    Resource Edit, ItemWall
    Monster, Item Resource Edit
    
    ItemWall 충돌 시 아이템 생성 + Sprite 변경

---
+++ 2D_Mario/Assets/Scripts/ItemWall.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemWall : MonoBehaviour
+{
+    public GameObject[] prefabs;
+
+    Animator anim;
+
+
+    bool bItemOn = true;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player" && bItemOn)
+        {
+            bItemOn = false;
+
+            anim.SetBool("IsItemOff", true);
+
+            int iselection = Random.Range(0, prefabs.Length);
+
+            GameObject selectedPrefab = prefabs[iselection];
+
+            Vector2 spawnPos = transform.position;
+
+            GameObject instance = Instantiate(selectedPrefab, spawnPos, Quaternion.identity);
+
+        }
+    }
+}
