22da0062264568d98cae7c7b743993b870fb397e
dbsrudekf <88525042+dbsrudekf@users.noreply.github.com>
2022-01-29 23:57:35 +0900

    MushRoomItem AI
    MushRoomItem AI 보완

--- 2D_Mario/Assets/Scripts/MushRoomItem.cs
+++ 2D_Mario/Assets/Scripts/MushRoomItem.cs
@@ -6,12 +6,19 @@ public class MushRoomItem : MonoBehaviour
 {
     float fSpeed = 5.0f;
     bool bIsPattern = false;
+    Vector3 movement;
+
+
     Rigidbody2D rigid;
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
     }
-
+    void Start()
+    {
+        Create();
+ 
+    }
     // Update is called once per frame
     void Update()
     {
@@ -20,22 +27,41 @@ public class MushRoomItem : MonoBehaviour
 
     void FixedUpdate()
     {
-        if(transform.position.y < -0.7f)
-        {
-            rigid.velocity = new Vector2(rigid.velocity.x, 0.5f);
-            Debug.Log("UP");
-            bIsPattern = true;
-        }
-        else
-        {
-            rigid.velocity = new Vector2(-1, rigid.velocity.y);
-        }
+        //movement.Set(0f, 1f, 0f);
+        //movement = movement.normalized * 1.0f * Time.deltaTime;
+        //rigid.MovePosition(transform.position + movement);
+        //if (transform.position.y > -0.7f)
+        //{
+        //    movement.Set(-1f, 0f, 0f);
+        //    movement = movement.normalized * 1.0f * Time.deltaTime;
+        //    rigid.MovePosition(transform.position + movement);
+        //}
+       
+        //if (transform.position.y > -0.7f)
+        //{
+        //    rigid.velocity = new Vector2(-1, 0);
+        //}
+        //if (transform.position.y < -0.7f)
+        //{
+        //    rigid.velocity = new Vector2(rigid.velocity.x, 0.5f);
+        //    Debug.Log("UP");
+        //    bIsPattern = true;
+        //}
         
         //if(transform.localPosition.y >= 0.15f)
         //{
         //    rigid.velocity = new Vector2(-1, rigid.velocity.y);
         //}
        
+    }
+
+    void Create()
+    {
+        for(float Itemposition= 1.0f; transform.position.y < -0.7f; Itemposition++)
+        {
+            rigid.velocity = new Vector2(rigid.velocity.x, 0.5f);
+            Debug.Log("UP");
+        }
     }
     void MovePattern()
     {
